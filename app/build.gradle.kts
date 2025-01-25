e6e7937fa4dc2147c3296ab52d4e008b304f61f6
adk23333 <2633103794@qq.com>
2025-01-25 15:40:13 +0800

    📦 build: 更新应用版本信息
    - 🔢 将 versionCode 从 3 修改为 4
    - 🏷️ 将 versionName 从 "2.1" 修改为 "2.2"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = android.namespace
         minSdk = 28
         targetSdk = 34
-        versionCode = 3
-        versionName = "2.1"
+        versionCode = 4
+        versionName = "2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
